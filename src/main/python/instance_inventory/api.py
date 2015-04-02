61df022ea29b55eb5a8c9b750a4866bd8d9f3ef2
Marco Hoyer <marco@hoyer.pm>
2015-04-02 23:31:06 +0200

    add logging and random app secret

--- src/main/python/instance_inventory/api.py
+++ src/main/python/instance_inventory/api.py
@@ -1,3 +1,6 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_restful import Api
 from instance_inventory.resource.instances import Instance, Instances
@@ -5,13 +8,28 @@ from instance_inventory.resource.instances import Instance, Instances
 app = Flask(__name__)
 api = Api(app)
 
+
+# set app secret to something considered random from os
+app.secret_key = os.urandom(24)
+
+
+# api resources
 api.add_resource(Instances, '/instances')
 api.add_resource(Instance, '/instances/<string:instance_id>')
 
 
-def run(bind, port, debug):
+def init_access_log(access_log_file):
+    logger = logging.getLogger('werkzeug')
+    handler = RotatingFileHandler(access_log_file, maxBytes=10000, backupCount=5)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+
+def run(bind, port, debug, access_log_file=False):
+    if access_log_file:
+        init_access_log(access_log_file)
     app.run(bind, port, threaded=True, debug=debug)
 
 
 if __name__ == '__main__':
-    run(bind='0.0.0.0', port=8080, debug=True)
+    run(bind='127.0.0.1', port=8080, debug=True)
--- src/main/scripts/instance-inventory-api.py
+++ src/main/scripts/instance-inventory-api.py
@@ -6,12 +6,14 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--bind', help="Bind address (0.0.0.0 f.e.)", type=str, default='0.0.0.0')
     parser.add_argument('--port', help="Port to bind on", type=int, default=8080)
-    parser.add_argument('--debug', help="Synchronisiere alle Shops", action="store_true", default=False)
+    parser.add_argument('--accesslog', help="Access log file", type=str, default=None)
+    parser.add_argument('--debug', help="Enable flask debug mode with integrated debugger (Don't use this on public!)",
+                        action="store_true", default=False)
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    api.run(args.bind, args.port, args.debug)
+    api.run(args.bind, args.port, args.debug, access_log_file=args.accesslog)
 
 main()
